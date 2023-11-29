7dbcff2f963896325914f07b77cd6233703acbba
Rob Bateman <rob@infiniteturtles.co.uk>
2023-11-29 02:47:54 +0000

    Add and Set function to b2MassData

--- lib/Box2Dold/Collision/Shapes/b2MassData.as
+++ lib/Box2Dold/Collision/Shapes/b2MassData.as
@@ -32,6 +32,37 @@ public class b2MassData
 	public var center:b2Vec2 = new b2Vec2(0,0);
 	/// The rotational inertia of the shape.
 	public var I:Number = 0.0;
+	
+	public static function Add(...masses):b2MassData
+	{
+		var finalMass:b2MassData=new b2MassData();
+		for each(var massData:b2MassData in masses){
+			finalMass.mass += massData.mass;
+		}
+		//b2Settings.b2Assert(finalMass.mass>=0);
+		if(Math.abs(finalMass.mass)>Number.MIN_VALUE){
+			for each(massData in masses){
+				finalMass.center.x += massData.mass*massData.center.x;
+				finalMass.center.y += massData.mass*massData.center.y;
+			}
+			finalMass.center.x /= finalMass.mass;
+			finalMass.center.y /= finalMass.mass;
+			for each(massData in masses){
+				finalMass.I += massData.I;
+				var r:b2Vec2 = b2Math.SubtractVV(massData.center, finalMass.center);
+				finalMass.I += massData.mass * b2Math.b2Dot(r, r);
+			}
+		}
+		return finalMass;
+	}
+	
+	public function Set(massData:b2MassData):void
+	{
+		mass = massData.mass;
+		center.x = massData.center.x;
+		center.y = massData.center.y;
+		I = massData.I;
+	}
 }
 
 }
--- lib/Box2Dold/Collision/Shapes/b2MassData.ts
+++ lib/Box2Dold/Collision/Shapes/b2MassData.ts
@@ -16,7 +16,7 @@
 * 3. This notice may not be removed or altered from any source distribution.
 */
 
-import { b2Vec2 } from '../../Common/Math';
+import { b2Math, b2Vec2 } from '../../Common/Math';
 
 /// This holds the mass data computed for a shape.
 export class b2MassData {
@@ -26,4 +26,35 @@ export class b2MassData {
 	public center: b2Vec2 = new b2Vec2(0,0);
 	/// The rotational inertia of the shape.
 	public I: number = 0.0;
+
+	public static Add(...masses: b2MassData[]):b2MassData
+	{
+		var finalMass:b2MassData=new b2MassData();
+		for (let massData of masses){
+			finalMass.mass += massData.mass;
+		}
+		//b2Settings.b2Assert(finalMass.mass>=0);
+		if(Math.abs(finalMass.mass)>Number.MIN_VALUE){
+			for (let massData of masses){
+				finalMass.center.x += massData.mass*massData.center.x;
+				finalMass.center.y += massData.mass*massData.center.y;
+			}
+			finalMass.center.x /= finalMass.mass;
+			finalMass.center.y /= finalMass.mass;
+			for (let massData of masses){
+				finalMass.I += massData.I;
+				var r:b2Vec2 = b2Math.SubtractVV(massData.center, finalMass.center);
+				finalMass.I += massData.mass * b2Math.b2Dot(r, r);
+			}
+		}
+		return finalMass;
+	}
+	
+	public Set(massData:b2MassData):void
+	{
+		this.mass = massData.mass;
+		this.center.x = massData.center.x;
+		this.center.y = massData.center.y;
+		this.I = massData.I;
+	}
 }
